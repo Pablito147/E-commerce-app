3eb293ddf7ad26215987bee278cd804a9f1568a3
Pavel <pavol.prcin@gmail.com>
2021-02-08 16:44:34 +0100

    finished sign-in and sign-out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-btn/custom-btn.component';
 
-import { signUpWithGoole } from '../../Firebase/firebase-utils';
+import { signUpWithGoole, auth } from '../../Firebase/firebase-utils';
 
 
 class SignIn extends React.Component {
@@ -16,10 +16,16 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error)
+        }
     };
     handleChange = event => {
         const { value, name } = event.target;
