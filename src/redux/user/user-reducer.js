2566dc8aabd30dc22eaef185f3d6ef3ef8b83145
Pavel <pavol.prcin@gmail.com>
2021-02-21 18:38:05 +0100

    added redirect app after sign in

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,10 +1,15 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
     currentUser: null
 }
 const userReducer = (state = INITIAL_STATE, acction) => {
     switch (acction.type) {
-        case 'SET_CURRENT_USER':
-            return { ...state, currentUser: acction.payload }
+        case UserActionTypes.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: acction.payload
+            };
 
         default:
             return state;
